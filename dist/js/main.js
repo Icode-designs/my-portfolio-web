26fbe937a7b62a9003f1ef7aed2d3c3fab50998c
Icode-designs <i.coder.6969@gmail.com>
2024-12-02 10:15:24 +0100

    main.js, menu button rotate

---
+++ dist/js/main.js
@@ -0,0 +1,36 @@
+//select dom items
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
+
+// set initial state of menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  console.log("called toggle");
+  if (!showMenu) {
+    console.log("first condition met");
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+
+    navItems.forEach((item) => item.classList.add("show"));
+
+    showMenu = true;
+  } else {
+    console.log("second condition met");
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+
+    navItems.forEach((item) => item.classList.remove("show"));
+
+    showMenu = false;
+  }
+}
